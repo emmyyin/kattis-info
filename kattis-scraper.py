81f20b251c46f554ba665e5c125b4a0f2d03b61e
Emmy Yin <emmy.yins@gmail.com>
2020-12-25 22:09:03 +0100

    hide password

--- kattis-scraper.py
+++ kattis-scraper.py
@@ -1,6 +1,7 @@
+import getpass
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
 url = "https://open.kattis.com"
 
@@ -15,22 +16,24 @@ login_forms = {
 }
 # loginform = login_forms[int(input("Choose login form: \n 0: Azure \n 1: Facebook \n 2: Github \n 3: Google \n 4: LinkedIn \n 5: email \n"))]
 
+username = input("Enter your google email or phone number:\n")
+password = getpass.getpass(prompt="Enter your google password:\n")
 score = ""
 ranking = ""
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
 driver.get(url)
 
 driver.find_element_by_xpath('//*[@id="wrapper"]/header/div/div/div[2]/nav/ul/li[2]/a').click()
 driver.find_element_by_xpath('//*[@id="wrapper"]/div/div/section/div[2]/div[2]/div/form[4]/button').click()
 
 # Login to Kattis through Google
-driver.find_element_by_id("identifierId").send_keys(input("Enter your google email or phone number:\n"))
-driver.find_element_by_id("identifierNext").click()
-time.sleep(2)
-driver.find_element_by_name("password").send_keys(input("Enter your google password:\n"))
+driver.find_element_by_xpath('//*[@id="identifierId"]').send_keys(username)
+driver.find_element_by_xpath('//*[@id="identifierNext"]/div/button').click()
+time.sleep(1)
+driver.find_element_by_xpath('//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
 driver.find_element_by_id("passwordNext").click()
-time.sleep(2)
+time.sleep(1)
 
 score = driver.find_element_by_xpath('//*[@id="wrapper"]/div/div[1]/section/div/div/div[2]/div/table/tbody/tr[2]/td[2]').text
 ranking = driver.find_element_by_xpath('//*[@id="wrapper"]/div/div[1]/section/div/div/div[2]/div/table/tbody/tr[2]/td[1]').text
